aba9197d73e0f9102699c384444fc25b3e2cc3b3
vlad <vlad@debian.debian>
2022-09-11 19:52:11 +0700

    Add error messages to sign up page

--- app/helpers/users/devise_helper.rb
+++ app/helpers/users/devise_helper.rb
@@ -1,2 +1,8 @@
 module Users::DeviseHelper
+    def devise_error_messages
+        if resource.errors.present?
+            error = resource.errors.full_messages.first.split(".")[-2..-1].join("_")
+            t("devise_errors.#{error}")
+        end
+    end
 end
