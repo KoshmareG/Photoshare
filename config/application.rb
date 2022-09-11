8400e2aa5650599437e502471d09c1b3240a24a1
vlad <vlad@debian.debian>
2022-09-11 19:50:54 +0700

    Include helpers on controllers

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Photoshare
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.action_controller.include_all_helpers = true
+
     config.i18n.default_locale = :ru
 
     # Configuration for the application, engines, and railties goes here.
