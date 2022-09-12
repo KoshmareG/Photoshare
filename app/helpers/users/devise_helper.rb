2dd9e09afb269b5b6a89083264e6c41c1cf6b4cb
vlad <vlad@debian.debian>
2022-09-12 17:26:25 +0700

    Update devise error method

--- app/helpers/users/devise_helper.rb
+++ app/helpers/users/devise_helper.rb
@@ -1,7 +1,7 @@
 module Users::DeviseHelper
     def devise_error_messages
         if resource.errors.present?
-            error = resource.errors.full_messages.first.split(".")[-2..-1].join("_")
+            error = resource.errors.first.attribute.to_s + "_" + resource.errors.first.type.to_s
             t("devise_errors.#{error}")
         end
     end
