da6cb75a326fd516f94cc01a5e43f101e8609150
vlad <vlad@debian.debian>
2022-09-13 14:42:29 +0700

    Js function for dropdowns

---
+++ app/javascript/custom/main.js
@@ -0,0 +1,18 @@
+function dropdown(elementId) {
+    document.getElementById(elementId).classList.toggle("is-active");
+}
+
+window.dropdown = dropdown;
+
+window.onclick = function(event) {
+    if (!event.target.matches('.button')) {
+        var dropdowns = document.getElementsByClassName("dropdown");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("is-active")) {
+                openDropdown.classList.remove("is-active");
+            }
+        }    
+    }
+}
