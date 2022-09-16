b9b2badc03b318fe7f4a09ce80bad7a4b66415d4
KoshmareG <koshmareg@yandex.ru>
2022-09-17 00:30:10 +0700

    Add home page with all users photo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
     before_action :authenticate_user!
 
     def show
+        @photos = Photo.order(likes_count: :desc).where("created_at >= ?", 1.week.ago.utc).where.not(user_id: current_user.id)
     end
     
 end
--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -6,4 +6,8 @@ module PhotosHelper
             t("errors.new_photo.photo_missing")
         end
     end
+
+    def show_all_photo photo
+        link_to (image_tag(photo.images.first.variant(resize_to_fill: [700, 700]))), photo_path(photo), style: "display: flex"
+    end
 end
