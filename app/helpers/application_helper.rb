f8c89eb473a7ba61120aea4a1c19cc1ff5416a1d
KoshmareG <koshmareg@yandex.ru>
2022-09-21 23:32:15 +0700

    Add support methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def declension count
+        if (count % 100 >= 11 && count % 100 <= 14) || (count % 10 == 0) || (count % 10 >= 5 && count % 10 <= 9)
+            "third"
+        elsif count % 10 == 1
+            "first"
+        elsif
+            "second"
+        end
+    end
+
+    def separator item
+        item.digits(1000).reverse.join(",")
+    end
 end
