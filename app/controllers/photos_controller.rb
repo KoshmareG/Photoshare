610c0a1fdcbcc6b90e8b1ec73024bd3a7507a441
KoshmareG <koshmareg@yandex.ru>
2022-09-22 22:21:36 +0700

    Add create hashtags

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -18,10 +18,14 @@ class PhotosController < ApplicationController
 
     def create
         @photo = Photo.new(photo_params)
-        @photo.user_id = current_user.id
+        @photo.user_id = current_user.id     
 
         if @photo.images.attached? && @photo.valid?
             @photo.save
+
+            hashtags = helpers.hashtag_parser photo_params[:text]
+            hashtags.each { |hashtag| @photo.hashtags.create(text: hashtag.downcase) }
+
             redirect_to @photo
         else
             flash[:alert] = helpers.new_photo_error @photo
