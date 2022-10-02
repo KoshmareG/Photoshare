c290ca6a75b99592ab491bc1b12733898664178e
KoshmareG <koshmareg@yandex.ru>
2022-10-02 23:19:17 +0700

    Refactoring aplication controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
-    def declension count
-        if (count % 100 >= 11 && count % 100 <= 14) || (count % 10 == 0) || (count % 10 >= 5 && count % 10 <= 9)
-            "third"
-        elsif count % 10 == 1
-            "first"
-        else
-            "second"
-        end
+  def declension count
+    if ((11..14).include? count % 100) || (count % 10 == 0) || ((5..9).include? count % 10)
+      "third"
+    elsif count % 10 == 1
+      "first"
+    else
+      "second"
     end
+  end
 
-    def separator item
-        item.digits(1000).reverse.join(",")
-    end
+  def separator item
+    item.digits(1000).reverse.join(",")
+  end
 end
