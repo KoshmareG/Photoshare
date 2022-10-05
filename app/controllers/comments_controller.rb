d8b6a37b8a2413eae6ac4cf9cc8da49c3fc9d157
KoshmareG <koshmareg@yandex.ru>
2022-10-06 00:04:29 +0700

    Add create comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+  def create
+    @photo = Photo.find(params[:photo_id])
+    @comment = @photo.comments.create(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.valid?
+      @comment.save
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text, :parent)
+  end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :parent, class_name: 'Comment', optional: true
   has_many :comments, foreign_key: :parent_id
+
+  validates :text, presence: true, length: { maximum: 300 }
+
+  default_scope { order(id: :desc) }
 end
