eeab8a1fb9facd7ca8496f7ecf42d0f77ddbeb42
KoshmareG <koshmareg@yandex.ru>
2022-10-21 22:48:42 +0700

    Add display last comments of posts on home page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,51 +1,51 @@
 class PhotosController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    before_action do
-        ActiveStorage::Current.url_options = { protocol: request.protocol, host: request.host, port: request.port }
+  before_action do
+    ActiveStorage::Current.url_options = { protocol: request.protocol, host: request.host, port: request.port }
+  end
+
+  def index
+    @posts = Photo.includes(:user, comments: [:user]).where(user_id: current_user.subscribes.pluck(:user_subscribe)).order(created_at: :desc)
+  end
+
+  def show
+    @photo = Photo.includes(:user, comments: [:user]).find(params[:id])
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.user_id = current_user.id     
+
+    if @photo.images.attached? && @photo.valid?
+      @photo.save
+
+      hashtags = helpers.hashtag_parser photo_params[:text]
+      hashtags.each { |hashtag| @photo.hashtags.create(text: hashtag.downcase) }
+
+      redirect_to @photo
+    else
+      flash[:alert] = helpers.new_photo_error @photo
+      render action: "new", status: :unprocessable_entity
     end
+  end
 
-    def index
-        @posts = Photo.includes(:user).where(user_id: current_user.subscribes.pluck(:user_subscribe)).order(created_at: :desc)
-    end
+  def edit
+  end
 
-    def show
-        @photo = Photo.includes(:user, comments: [:user]).find(params[:id])
-    end
+  def update
+  end
 
-    def new
-        @photo = Photo.new
-    end
+  def destroy
+  end
 
-    def create
-        @photo = Photo.new(photo_params)
-        @photo.user_id = current_user.id     
+  private
 
-        if @photo.images.attached? && @photo.valid?
-            @photo.save
-
-            hashtags = helpers.hashtag_parser photo_params[:text]
-            hashtags.each { |hashtag| @photo.hashtags.create(text: hashtag.downcase) }
-
-            redirect_to @photo
-        else
-            flash[:alert] = helpers.new_photo_error @photo
-            render action: "new", status: :unprocessable_entity
-        end
-    end
-
-    def edit
-    end
-
-    def update
-    end
-
-    def destroy
-    end
-
-    private
-
-    def photo_params
-        params.require(:photo).permit(:text, images: [])
-    end
+  def photo_params
+    params.require(:photo).permit(:text, images: [])
+  end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,10 @@
 class Photo < ApplicationRecord
-    belongs_to :user
-    has_many_attached :images
-    has_many :hashtags, dependent: :destroy
-    has_many :likes, dependent: :destroy
-    has_many :comments, dependent: :destroy
+  belongs_to :user
+  has_many_attached :images
+  has_many :hashtags, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :last_comments, -> { limit(3) }, class_name: 'Comment'
 
-    validates :text, length: { maximum: 1200 }
+  validates :text, length: { maximum: 1200 }
 end
