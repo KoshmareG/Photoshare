5c16e41f627c5667a4b35873b2c6ed2b6eb7bbda
KoshmareG <koshmareg@yandex.ru>
2022-10-21 23:59:13 +0700

    Add link to show all comments of photo on home page

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,8 @@
 module HomeHelper
+  def post_comments(post)
+    case post.comments.size
+    when 0 then "#{t("home_page.new_comment")}"
+    else "#{t("home_page.show_all_comments")} (#{post.comments.size.to_s})"
+    end
+  end
 end
